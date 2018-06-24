41edd0b72aca09156d5633ad5e6c79ae1d2da689
choisound <choisound@qq.com>
2018-06-24 12:02:16 +0800

    first commit

---
+++ src/main/java/com/cyx/concurrucy/ConcurrucyApplication.java
@@ -0,0 +1,12 @@
+package com.cyx.concurrucy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrucyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrucyApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cyx/concurrucy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cyx.concurrucy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConcurrucyApplication.class);
+	}
+
+}
